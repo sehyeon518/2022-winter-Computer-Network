56d8dd4c81303e7e7f8b340f2fa2e4c869b7c0a4
sehyeon518 <lifethis21@kookmin.ac.kr>
2023-01-03 14:08:32 +0900

    'accept' success

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
 from socket import *
 
-serverName = gethostname() # server
+serverName = '127.0.0.1' # server
 print(serverName)
-serverPort = 80 # HTTP
-clientSocket = socket(AF_INET, SOCK_STREAM) # client 소켓 생성
+serverPort = 9541 # HTTP port number
 
-clientSocket.connect((serverName, serverPort)) # 
-print('server 접속')
+while True:
+    clientSocket = socket(AF_INET, SOCK_STREAM) # client 소켓 생성 (TCP)
 
-clientSocket.send('who is nerdy'.encode())
-print('send message')
-data = clientSocket.recv(1024) # server name, port에 접근할 필요 없음 -> socket 사용
-print('From Server:', data.decode()) # write reply
-clientSocket.close()
-print('종료')
+    clientSocket.connect((serverName, serverPort)) # (IP, port) 서버 연결
+    print('server 접속')
+
+    message = input('Request>> ')
+    message = message.split()
+    print(message)
+
+    if message[0] == 'GET' or message[0] == 'HEAD' or message[0] == 'POST' or message[0] == 'PUT':
+        header = " ".join(message)
+    else:
+        print("Invalid method")
+        print("종료")
+        break
+
+    clientSocket.send(header.encode()) # sockt으로 header 보내기
+    print('send message:', header)
+    print('response 대기')
+
+    response = clientSocket.recv(1024).decode() # server name, port에 접근할 필요 없음 -> socket 사용
+
+    clientSocket.close()
+    print('연결 해제')
--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 from socket import *
 
-serverPort = 80
+serverPort = 9541
 serverSocket = socket(AF_INET, SOCK_STREAM) # TCP 방식의 socket 생성
-serverSocket.bind(('locslhost', serverPort)) # (IP, port) tuple
+serverSocket.bind(('', serverPort)) # (IP, port) tuple
 
 serverSocket.listen(0) # port listening. 동시에 연결 가능한 socket 개수는 한개
 print('The server is ready to receive') # 확인
+print('accept 대기')
 
-connectionSocket, addr = serverSocket.accept() # 클라이언트 접속할 때까지 대기
-print('accept') # accpet가 되면 cennection setup
+while True:
+    connectionSocket, addr = serverSocket.accept() # 클라이언트 접속할 때까지 대기
+    print('*****accept*****') # accpet가 되면 cennection setup
 
-data = connectionSocket.recv(1024) #클라이언트 접속이 되면 데이터를 읽어들임.
-print('print', data.decode())
-connectionSocket.send(data) # socket 보내기
-print('send')
-connectionSocket.close() # 연결 종료
-print('close')
+    while True:
+        message = connectionSocket.recv(1024) #클라이언트 접속이 되면 데이터를 읽어들임.
+        request = message.decode()
+        print('request', request)
+
+        request = request.split()
+        print(request)
+
+        connectionSocket.send(message) # socket 보내기
+        print('send')
+        connectionSocket.close() # 연결 종료
+        print('close')
