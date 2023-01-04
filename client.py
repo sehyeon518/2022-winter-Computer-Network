999d2241969b94fd82997cd5fef999baee8d10c6
sehyeon518 <lifethis21@kookmin.ac.kr>
2023-01-04 13:27:00 +0900

    try1

--- client.py
+++ client.py
@@ -1,31 +1,31 @@
 from socket import *
 
 serverName = '127.0.0.1' # server
-print(serverName)
-serverPort = 9541 # HTTP port number
+serverPort = 1230 # port number
 
 while True:
-    clientSocket = socket(AF_INET, SOCK_STREAM) # client 소켓 생성 (TCP)
-
+    clientSocket = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM으로 TCP 방식의 client socket 생성
     clientSocket.connect((serverName, serverPort)) # (IP, port) 서버 연결
-    print('server 접속')
+    print('********server 연결********')
 
-    message = input('Request>> ')
+    message = input('Request>> ') # 사용자로부터 request 입력 받기
     message = message.split()
-    print(message)
 
-    if message[0] == 'GET' or message[0] == 'HEAD' or message[0] == 'POST' or message[0] == 'PUT':
-        header = " ".join(message)
+    if message[0] == 'GET' or message[0] == 'HEAD' or message[0] == 'POST':
+        request = message[0] + " "
+        if len(message) > 1:
+            request += " ".join(message[1:])
+        request += " / HTTP/1.0\nHOST: " + serverName
     else:
         print("Invalid method")
-        print("종료")
-        break
+        request = " ".join(message)
 
-    clientSocket.send(header.encode()) # sockt으로 header 보내기
-    print('send message:', header)
-    print('response 대기')
+    # request 형태
+    # GET test.html * \ HTTP/1.0
+    # HOST: 127.0.0.1
+    clientSocket.send(request.encode()) # socket으로 request 보내기
 
     response = clientSocket.recv(1024).decode() # server name, port에 접근할 필요 없음 -> socket 사용
-
+    print(response)
     clientSocket.close()
-    print('연결 해제')
+    print('***********close***********')
--- server.py
+++ server.py
@@ -1,26 +1,54 @@
 from socket import *
 
-serverPort = 9541
-serverSocket = socket(AF_INET, SOCK_STREAM) # TCP 방식의 socket 생성
+f = open('test.html', 'r')
+r = f.read()
+
+serverPort = 1230 # port number
+serverSocket = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM으로 TCP 방식의 socket 생성
 serverSocket.bind(('', serverPort)) # (IP, port) tuple
 
-serverSocket.listen(0) # port listening. 동시에 연결 가능한 socket 개수는 한개
-print('The server is ready to receive') # 확인
-print('accept 대기')
+serverSocket.listen(0) # port listen, 동시에 연결 가능한 socket 개수, 0이면 자동
 
 while True:
-    connectionSocket, addr = serverSocket.accept() # 클라이언트 접속할 때까지 대기
-    print('*****accept*****') # accpet가 되면 cennection setup
+    connectionSocket, addr = serverSocket.accept() # client 접속할 때까지 대기
+    print('********client 연결********') # accpet가 되면 connection setup
 
-    while True:
-        message = connectionSocket.recv(1024) #클라이언트 접속이 되면 데이터를 읽어들임.
-        request = message.decode()
-        print('request', request)
+    message = connectionSocket.recv(1024) # client 접속이 되면 message 읽기
+    request = message.decode() # message 해독
+    print(request)
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    request = request.split()
 
-        request = request.split()
-        print(request)
+    # response 형태
+    # HTTP/1.0 200 OK
+    # GET이면 파일 전체 읽어오기, HEAD이면 
+    response = 'HTTP/1.0 '
+    # HEAD
+    if request[0] == 'HEAD':
+        if request[1] !=  "/": # 요청한 데이터가 추가로 있으면
+            response += '404 Not Found'
+        else:
+            response += '200 OK'
+    # GET
+    elif request[0] == 'GET':
+        if request[1] == 'test.html':
+            response += '200 OK\n' + r
+        else:
+            response += '404 Not Found'
+    # POST
+    elif request[0] == 'POST':
+        if request[1] != 'test.html' and "." in request[1]:
+            newfile = open(request[1], "w")
+            newread = newfile.read()
+            newfile.close()
+            response += '200 OK\n' + newread
+        else:
+            response += '404 Not Found'
+    else:
+        response += '400 Bad Request' # method 해석 불가능한 경우
 
-        connectionSocket.send(message) # socket 보내기
-        print('send')
-        connectionSocket.close() # 연결 종료
-        print('close')
+    connectionSocket.send(response.encode()) # socket 보내기
+    print(response)
+    
+    connectionSocket.close() # 연결 종료
+    print('***********close***********')
